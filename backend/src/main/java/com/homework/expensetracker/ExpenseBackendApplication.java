99ca80c026758fa16dd320eb03789bbbb474bd72
kannikakak <kakkannika896@gmail.com>
2026-02-23 14:51:11 +0700

    feat(backend): scaffold Spring Boot Maven service

---
+++ backend/src/main/java/com/homework/expensetracker/ExpenseBackendApplication.java
@@ -0,0 +1,12 @@
+package com.homework.expensetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ExpenseBackendApplication.class, args);
+  }
+}
---
+++ backend/src/test/java/com/homework/expensetracker/ExpenseBackendApplicationTests.java
@@ -0,0 +1,12 @@
+package com.homework.expensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
